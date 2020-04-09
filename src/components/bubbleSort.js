946911da27e729ebce32179f92e9de2dcb4dd314
gm_desktop <gmesda23@gmail.com>
2020-04-09 14:13:55 -0400

    added small comment explaining how the algo works for changing css color

--- src/components/bubbleSort.js
+++ src/components/bubbleSort.js
@@ -1,5 +1,12 @@
 
-
+//  the array uses i to check what child we 
+// are targeting from array-circles class, since its  
+// redner in a map, then we log each change or move from
+//  the sort into an array and then place those moves in an
+//   equation that changes the background css according to the moves that happened.
+//  uses math we can say that the final 3rd array within the array, 
+// of every 3 moves is what the solution should be and use that to change the 
+// css of the nth child div 
 export function getBubbleSortAnimations(array) {
     const animations = [];
     // let inputArr = array;
