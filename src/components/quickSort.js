d8958ec068ed217dd88d9c55b1f68ffe789272d3
gm_desktop <gmesda23@gmail.com>
2020-04-11 20:10:07 -0400

    updating quicksort

--- src/components/quickSort.js
+++ src/components/quickSort.js
@@ -1,7 +1,8 @@
 export function getQuickSortAnimations(array) {
 
     console.log(array, ' array fromqs')
-    return quickSort(array);
+    let newArr = quickSort(array)
+    return newArr;
 
 }
 
@@ -24,13 +25,14 @@ function quickSort(arr) {
     // our pivot and place it on a left array, if its equal to or greater we place it on the 
     // right array 
     for (let i = 0; i < arr.length - 1; i++) {
+        animations.push([i, pivot])
         if (arr[i] < pivot) {
             leftArr.push(arr[i]);
-            animations.push([i, i]);
+            animations.push([i, ...leftArr]);
             
         } else {
             rightArr.push(arr[i]);
-            animations.push([i, i]);
+            animations.push([i, ...rightArr]);
 
         }
     }
@@ -38,25 +40,23 @@ function quickSort(arr) {
     // As long as the left array and right array is greater than one element we keep 
     // running quicksort recusively
     if (leftArr.length > 0 && rightArr.length > 0) {
-        animations.push([...quickSort(leftArr), pivot, ...quickSort(rightArr)]);
-        // animations.push([pivot, arr[i]]);
+    
+        // animations.push([pivot]);
 
 
         return [...quickSort(leftArr), pivot, ...quickSort(rightArr)], animations;
         // our pivot can be the last element so we keep quick sorting until the left array length is zero
     } else if (leftArr.length > 0) {
-        animations.push([...quickSort(leftArr), pivot]);
+        // animations.push([...quickSort(leftArr), pivot]);
 
         // animations.push([leftArr, rightArr]);
-
+      
 
         return [...quickSort(leftArr), pivot], animations;
         // our pivot could have move to the front so we have conditional to quick sort to the right array as well
     } else {
         // animations.push([leftArr, rightArr]);
-        animations.push([pivot, ...quickSort(rightArr)]);
-
-
+        // animations.push([pivot, ...quickSort(rightArr)]);
 
         return [pivot, ...quickSort(rightArr)], animations;
     }
