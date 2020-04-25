b36e8f7373608a7bb3d6af19e86f54690844ba6b
gm_desktop <gmesda23@gmail.com>
2020-04-25 00:18:07 -0400

    heroku

---
+++ build/precache-manifest.31c0e25c1459f85b44465fc27dd83e94.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fcb86e68157031ca666fee58de75fa3",
+    "url": "/build/index.html"
+  },
+  {
+    "revision": "656d07490f2fca45a8ea",
+    "url": "/build/static/css/2.35013e12.chunk.css"
+  },
+  {
+    "revision": "c05e22d22ae0968a622b",
+    "url": "/build/static/css/main.d590b820.chunk.css"
+  },
+  {
+    "revision": "656d07490f2fca45a8ea",
+    "url": "/build/static/js/2.01d45083.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/build/static/js/2.01d45083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c05e22d22ae0968a622b",
+    "url": "/build/static/js/main.a1ae876a.chunk.js"
+  },
+  {
+    "revision": "8fac40646430e9df439c",
+    "url": "/build/static/js/runtime-main.39ff2ee5.js"
+  },
+  {
+    "revision": "5ebb24ee1112dd9562629375c387879a",
+    "url": "/build/static/media/Roboto-Black.5ebb24ee.ttf"
+  },
+  {
+    "revision": "2923aa61e2dc8a0d1162fb74b4c22598",
+    "url": "/build/static/media/wotfard-regular-webfont.2923aa61.ttf"
+  }
+]);
